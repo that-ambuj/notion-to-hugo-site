07bbd1be4f14d496c19537bab0e77aefa7df09a4
Ambuj Singh <97109381+that-ambuj@users.noreply.github.com>
2023-11-18 14:21:00 +0530

    update for new version

--- index.js
+++ index.js
@@ -13,47 +13,65 @@ const databaseId = process.env["NOTION_DATABASE_ID"];
 const MARKDOWN_DIR = "./hugo-site";
 
 export async function downloadMarkdown() {
-  const n2m = new NotionToMarkdown({ notionClient: notion, staticFileDir: "hugo-site/static" });
+  const n2m = new NotionToMarkdown({
+    notionClient: notion,
+    staticFileDir: "hugo-site/static",
+  });
 
-  if (!databaseId) throw Error("Missing NOTION_DATABASE_ID environment variable.");
+  if (!databaseId)
+    throw Error("Missing NOTION_DATABASE_ID environment variable.");
 
   // Query the blog database
   const res = await notion.databases.query({
-    database_id: databaseId
+    database_id: databaseId,
   });
 
   console.log("Downloading Pages from Notion....");
 
-  await Promise.all(res.results.map(async (r) => {
-    const pageData = await notion.pages.retrieve({ page_id: r.id });
+  await Promise.all(
+    res.results.map(async (r) => {
+      const pageData = await notion.pages.retrieve({ page_id: r.id });
 
-    // @ts-ignore: JSON data is not typed in pageData
-    const tags = pageData?.properties?.Tags?.multi_select.map(t => t.name);
+      console.log(pageData.properties?.Name?.title);
 
-    // @ts-ignore: JSON data is not typed in pageData
-    const author = pageData?.properties?.Authors?.people[0]?.name;
+      // @ts-ignore: JSON data is not typed in pageData
+      const tags = pageData?.properties?.Tags?.multi_select.map((t) => t.name);
 
+      // @ts-ignore: JSON data is not typed in pageData
+      const author = pageData?.properties?.Authors?.people[0]?.name;
 
-    // @ts-ignore: JSON data is not typed in pageData
-    const title = pageData?.properties?.Name?.title[0]?.plain_text;
-    // @ts-ignore: JSON data is not typed in pageData
-    const publishedOn = pageData?.properties["Published On"].date?.start;
-    const slugifiedTitle = title.split(" ").join("-").toLowerCase();
+      // @ts-ignore: JSON data is not typed in pageData
+      const title = pageData?.properties?.Name?.title[0]?.plain_text;
 
-    const mdBlocks = await n2m.pageToMarkdown(r.id);
+      if (!title) {
+        return;
+      }
 
-    const mdString = n2m.toMarkdownString(mdBlocks);
+      // @ts-ignore: JSON data is not typed in pageData
+      const publishedOn = pageData?.properties["Published On"].date?.start;
 
-    // Front Matter is in TOML
-    const hugoHeader = `+++
+      const slugifiedTitle =
+        title?.split(" ").join("-").toLowerCase() ?? "title";
+
+      const mdBlocks = await n2m.pageToMarkdown(r.id);
+
+      const mdString = n2m.toMarkdownString(mdBlocks);
+
+      // Front Matter is in TOML
+      const hugoHeader = `+++
 title = "${title}"
-date = ${publishedOn}
-author = "${author ?? 'Anonymous'}"
+date = "${publishedOn}"
+author = "${author ?? "Anonymous"}"
 tags = ["${tags.join('", "')}"]
 +++`;
 
-    await fs.writeFile(`${MARKDOWN_DIR}/content/${slugifiedTitle || r.id}.md`, hugoHeader + mdString, "utf8");
-  }));
+      await fs.writeFile(
+        `${MARKDOWN_DIR}/content/${slugifiedTitle || r.id}.md`,
+        hugoHeader + mdString.parent,
+        "utf8",
+      );
+    }),
+  );
 }
 
 await downloadMarkdown();
